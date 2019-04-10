68abbec458bffe329b1bdc28e27a40a7b6d78d4b
Paul Irish <paulirish@google.com>
2019-04-10 13:26:04 -0700

    changedfiles script that wont work

--- .now-build.sh
+++ .now-build.sh
@@ -7,8 +7,15 @@ npm i details-element-polyfill@2.2.0
 
 mkdir -p dist
 
-git status
-ls
+# Ideally we early exit if no report stuff was affected. However deployments would still be made. They'd just fail or w/e.
+# we'd have to use NOW_GITHUB_COMMIT_SHA to find the PR to use
+#     https://zeit.co/docs/v2/integrations/now-for-github/#included-environment-variables
+
+# CHANGED_FILES=$(curl https://api.github.com/repos/GoogleChrome/lighthouse/pulls/8158 -H "Accept: application/vnd.github.v3.patch" | tr '\n' '$' | sed 's/diff --git.*//; s/.*---//;') # oddly couldnt get patch output to work on commits, though that's moot
+# if ! echo $CHANGED_FILES | grep -E 'report|audits|i18n' > /dev/null; then
+#   echo "No report files affected, skipping report generation."
+#   exit 0
+# fi
 
 # generate the report and place as dist/index.html
 node -e "
